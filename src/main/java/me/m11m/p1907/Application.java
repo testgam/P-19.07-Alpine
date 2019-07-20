8612c9c2ca7da308bd515a5f02bee46fcc8a5a30
Chulman Park <testgam1@gmail.com>
2019-07-20 12:28:27 +0900

    initial

---
+++ src/main/java/me/m11m/p1907/Application.java
@@ -0,0 +1,13 @@
+package me.m11m.p1907;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
